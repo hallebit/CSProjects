d007e75977b0ab0c50771394362828c0c9e1656c
typeHaller <halle@remash.com>
2014-09-09 09:45:01 -0700

    added Gdc method (untested)

--- CSProjectsFactors/src/Factors.java
+++ CSProjectsFactors/src/Factors.java
@@ -45,12 +45,34 @@ public class Factors {
 	 */
 	public static void getFactors(int num) {
 		System.out.println("Finding factors of the number " + num + "...");
+		String fac = "";
 		for(int i = num; i > 0; i--) {
 			if(num % i == 0) {
 				System.out.print(i + " ");
+				fac += i + "";
 			}
 		}
 		System.out.println();
+	}
+	
+	public static void getGDC(int num1, int num2) {
+		System.out.println("Finding the Greatest Common Denominator for " + num1 + " and " + num2);
+		String gdc = "";
+		if(num1 < num2) {
+			for(int i = num1; i >= 1; i--) {
+				if(num2 % i == 0 && num1 % i == 0) {
+					gdc = i + "";
+				}
+			}
+		}else if(num1 > num2) {
+			for(int i = num2; i >= 1; i--) {
+				if(num2 % i == 0 && num1 % i == 0) {
+					gdc = i + "";
+				}
+			}		
+		}else {
+			gdc = num1 + "";
+		}
 	}
 
 }
