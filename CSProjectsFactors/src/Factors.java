3c5d4b6be355065b4cf957b4fb2148c193ec7fc4
typeHaller <halle@remash.com>
2014-09-11 21:10:40 -0700

    Mostly Functional. Needs code to keep GDC numbers within 1 - 100 range. Needs Tests.

--- CSProjectsFactors/src/Factors.java
+++ CSProjectsFactors/src/Factors.java
@@ -12,104 +12,106 @@ public class Factors {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String action = getFacsOrGdcOrQuit();
-		System.out.println(action);
-		
-		
-		
-		/*boolean quit = false;
-		boolean error = false;
-		int hasNextInt = 0;
-		ArrayList<Integer> gdc = new ArrayList<Integer>();
-		int num = 0;
-		while(!quit) {
-			System.out.println("Pick a number between 1 and 100:");
-			while(console.hasNextInt()) {
-				hasNextInt++;
-				gdc.add(console.nextInt());
-			}
-			if(hasNextInt == 0) {
-				String input = console.next();
-			try {
-				num = Integer.parseInt(input);
-			} catch (NumberFormatException e) {
-				if(input.equalsIgnoreCase("quit")) {
-					quit = true;
-				} else {
-					System.out.println(e);
-					error = true;
-				}
-			}
-			if(num <= 0 || num > 100) {
-				if(quit || error) {} else {
-					System.out.println("Input not within a valid range");
-				}
-			} else {
-				getFactors(num);
-			}
-			}else
-			error = false;
-			gdc.clear();
-			hasNextInt = 0;
-			num = 0;
-		} 
-		System.out.print("Thanks for Playing"); */
+		getNumsOrQuit();
 	}
 	
+	//TODO: functionality
+	public static void getNumsOrQuit() {
+		System.out.println("Input one number between 1 and 100 to get all of its factors.");
+		System.out.println("Input multiple numbers to get the Greatest Common Denominator.");
+		System.out.println("Input 'quit' to quit the program.");
+		while(console.hasNextInt()) {
+			ArrayList<Integer> input = numOrNums(console.nextLine());
+			if(input.size() == 1) {
+				getFactors(input.get(0));
+			} else {
+				getGDC(input);
+			}
+		}
+		if(console.hasNext()) {
+			if(console.next().equalsIgnoreCase("quit")) {
+				System.out.println("Thanks for playing");
+			} else {
+				System.out.println("Error input invalid, try again.");
+				System.out.println();
+				getNumsOrQuit();
+			}
+		}	
+	}
+	
+	public static ArrayList<Integer> numOrNums(String toFind) {
+		ArrayList<Integer> toReturn = new ArrayList<Integer>();
+		String toSearch = toFind;
+		if(toSearch.contains(" ")) {
+			toSearch = toSearch + " ";
+			while(toSearch.contains(" ")) {
+				try {
+					toReturn.add(Integer.parseInt(toSearch.substring(0, toSearch.indexOf(" "))));
+					toSearch = toSearch.substring(toSearch.indexOf(" ") + 1);
+				} catch(NumberFormatException e) {
+					System.out.println("Error input invalid, try once more.");
+					System.out.println();
+					getNumsOrQuit();
+				}
+			}
+		} else {
+			try {
+				toReturn.add(Integer.parseInt(toFind));
+			} catch(NumberFormatException e) {
+				System.out.println("Error input invalid, please input data again");
+				System.out.println();
+				getNumsOrQuit();
+			}
+		}
+		return toReturn;
+	}
+		
 	/**
 	 * @param num
 	 */
 	public static void getFactors(int num) {
-		System.out.println("Finding factors of the number " + num + "...");
-		for(int i = num; i > 0; i--) {
-			if(num % i == 0) {
-				System.out.print(i + " ");
+		if(num <= 100 && num >= 1) {
+			System.out.println("Finding factors of the number " + num + "...");
+			for(int i = num; i > 0; i--) {
+				if(num % i == 0) {
+					System.out.print(i + " ");
+				}
 			}
+			System.out.println();
+		} else {
+			System.out.println("Input is not within a valid range, please try again");
+			System.out.println();
+			getNumsOrQuit();
 		}
-		System.out.println();
+		
 	}
 	
 	public static void getGDC(ArrayList<Integer> gdc) {
-		System.out.println("Finding the Greatest Common Denominator of " + gdc.toString());
+		System.out.println("Finding the Greatest Common Denominator of " + gdc.toString() + "...");
 		int GDC = 1;
+		boolean withinRange = true;
 		for(int i = 1; i <= getSmallest(gdc); i++) {
 			boolean isGDC = true;
 			for(int num = 0; num < gdc.size(); num++) {
-				if(gdc.get(num) % i != 0) {
-					isGDC = false;
+				if(gdc.get(num) <= 100 && gdc.get(num) >= 1) {
+					if(gdc.get(num) % i != 0) {
+						isGDC = false;
+					}
+				} else {
+					withinRange = false;
 				}
 			}
 			if(isGDC) {
 				GDC = i;
 			}
 		}
-		System.out.print("Greatest Common Denominator is " + GDC);		
-	}
-	
-	//TODO: functionality
-	public static String getFacsOrGdcOrQuit() {
-		System.out.println("Input one number between 1 and 100 to get all of its factors.");
-		System.out.println("Input multiple numbers to get the Greatest Common Denominator.");
-		System.out.println("Input 'quit' to quit the program.");
-		String input = "";
-		int i = 0;
-		if(console.hasNextInt()) {
-			while(console.hasNextInt()) {
-				i++;
-				input += i + " " + console.nextInt();
-			}
-			return input;
-		} else if(console.hasNext()) {
-			return "q" + console.next();
-		} else
-			return "Error input invalid";
-		
-	}
-	
-	//TODO: functionality
-	public static String getInput() {
-
-		return console.next();
+		if(withinRange) {
+			System.out.println("Greatest Common Denominator is " + GDC);		
+		} else {
+			System.out.println("Input is not within a valid range, please try again");
+			System.out.println();
+			getNumsOrQuit();
+		}
 	}
 	
 	public static int getSmallest(ArrayList<Integer> list) {
@@ -121,4 +123,5 @@ public class Factors {
 		}
 		return smallest;
 	}
+	
 }
